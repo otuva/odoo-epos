a11061caecd5066d92a337b5de77a54fdac80bea
xiaohao0576 <xiaohao0576@gmail.com>
2025-06-11 14:51:33 +0700

    move LoadPrintersConfig to main function

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ func init() {
 	ConfigFile = flag.String("c", "config.json", "Path to the configuration file")
 	Port = flag.String("p", "443", "Port to run the server on")
 	flag.Parse()
-	LoadPrintersConfig(*ConfigFile)
 }
 
 func main() {
+	LoadPrintersConfig(*ConfigFile)
 	StartHttpServer()
 }
